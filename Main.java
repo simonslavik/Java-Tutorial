21430b422a08fd8bed8bc4fb691709a8254c2faf
simonslavik <simonslavik007@gmail.com>
2025-12-11 16:42:25 +0100

    added code

--- Main.java
+++ Main.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Hello World");
+            System.out.println("Current date and time: " + new Date());
+        }
     }
 }
